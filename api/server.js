72793160f764cd30a49f1c90f3421eb9a88e4871
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-02-17 23:04:55 -0500

    Installed the necessary packages

--- api/server.js
+++ api/server.js
@@ -1,1 +1,11 @@
 // build your server here and require it from index.js
+const express = require('express');
+const cors = require('cors');
+const helmet  = require('helmet');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+module.exports = app;
